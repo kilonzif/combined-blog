e0a7a861bad87f4c4046bcc980ddb32454bfcf76
RhonaJoyKe <rhonajoy8@gmail.com>
2022-06-02 11:04:26 -0400

    removes views

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     #path, name of function, namespace 
     path('',views.home),
     path('upload/',views.upload,name='upload'),
-    path('update/<int:blog_id>',views.views.update_blog,name='update'),
-    path('delete/<int:blog_id>',views.views.delete_blog,name='delete')
+    path('update/<int:blog_id>',views.update_blog,name='update'),
+    path('delete/<int:blog_id>',views.delete_blog,name='delete')
     
 ]
